606f883c7bc781455536cd5663d2165dbb572bff
Sachin Maurya <sachin22424@iiitd.ac.in>
2024-11-13 05:03:58 +0530

    Home page completed

--- client_frontend/src/components/Navbar.jsx
+++ client_frontend/src/components/Navbar.jsx
@@ -23,79 +23,86 @@ const Navbar = () => {
 
     if (!user) {
         return (
-            <nav className="navbar navbar-expand-lg bg-dark navbar-not-logged-in">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-custom">
                 <div className="container-fluid">
-                    <a className="navbar-brand text-white" href="/">
-                        <img src={logo} alt="Icon" className="navbar-icon" />
+                    <a className="navbar-brand" href="#">
+                        <img src={logo} alt="Logo" className="logo-img" />
                     </a>
-                    <button className="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0" id="links" style={{ width: '20%' }}>
-                            <li className="nav-item1">
-                                <a className="nav-link active text-white mx-3" aria-current="page" href="/">Home</a>
+                        <ul className="navbar-nav mb-2 mb-lg-0 custom-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active " aria-current="page" href="#">Home</a>
                             </li>
-                            <li className="nav-item1">
-                                <a className="nav-link text-white mx-3" href="/about">About</a>
+                            <li className="nav-item dropdown custom-dropdown">
+                                <a className="nav-link " href="#" role="button">
+                                    Our Story
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Our Team</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Our Mission</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Our Initiatives</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">About Us</a></li>
+                                </ul>
                             </li>
-                            <li className="nav-item1">
-                                <a className="nav-link text-white mx-3" href="/contact">Contact</a>
+                            <li className="nav-item dropdown custom-dropdown">
+                                <a className="nav-link " href="#" role="button">
+                                    Our Work
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Yasham Centre Model</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Saathi Haath Badhana</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Sunn Zara</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Hum Honge Kaamyab</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Swacch English Mission</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown custom-dropdown">
+                                <a className="nav-link " href="#" role="button">
+                                    Our Impact
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Student Testimonials</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Mentor Testimonials</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Yasham in News</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown custom-dropdown">
+                                <a className="nav-link " href="#" role="button">
+                                    Get Involved
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Sponsor</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Support Us</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Teach</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Mentor</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active " aria-current="page" href="#">Contact Us</a>
                             </li>
                         </ul>
-                        <a href="/login" className="btn btn-outline-light login-btn" style={{ width: '80px' }}>Login</a>
                     </div>
                 </div>
             </nav>
         );
-    } else {
-        return (
-            <div className="navbar-container">
-                <div className="navbar">
-                    <div className="navbar-header">
-                        <img src={logo} alt="Logo" className="navbar-logo" />
-                        <div className="navbar-profile">
-                            <FaUser className="navbar-icon" />
-                            <span>{user.name}</span>
-                        </div>
-                    </div>
-                    <ul className="navbar-nav mt-4">
-                        <li className="nav-item">
-                            <a href="/" style={{ color: 'inherit', textDecoration: 'none' }}>
-                                <FaComments className="navbar-icon" />
-                                <span>Chats</span>
-                            </a>
-                        </li>
-                        <Notifications soundEnabled={soundEnabled} />
-                        <li className="nav-item">
-                            <a href="/about" style={{ color: 'inherit', textDecoration: 'none' }}>
-                                <FaInfoCircle className="navbar-icon" />
-                                <span>About</span>
-                            </a>
-                        </li>
-                        <li className="nav-item mb-4">
-                            <a href="/contact" style={{ color: 'inherit', textDecoration: 'none' }}>
-                                <FaQuestionCircle className="navbar-icon" />
-                                <span>Help</span>
-                            </a>
-                        </li>
-                        <li className="nav-item logout mt-4" onClick={handleLogout}>
-                            <a href="/logout" style={{ color: 'inherit', textDecoration: 'none' }}>
-                                <FaSignOutAlt className="navbar-icon" />
-                                <span>Logout</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <div className="nav-link sound-icon" onClick={toggleSound}>
-                                {soundEnabled ? <FaVolumeUp className="navbar-icon white-color" /> : <FaVolumeMute className="navbar-icon white-color" />}
-                                <span className="white-color">{soundEnabled ? "on" : "off"}</span>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        );
     }
+
+    return null;
 };
 
 export default Navbar;
--- client_frontend/src/pages/Chat.jsx
+++ client_frontend/src/pages/Chat.jsx
@@ -1,87 +1,173 @@
 import React, { useContext } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCommentDots } from "@fortawesome/free-solid-svg-icons";
-import "../assets/Chat.css";
-import slider1 from '../assets/slider1.png';
+import "font-awesome/css/font-awesome.min.css"; // Import Font Awesome
+import slider1 from '../assets/slider1.jpg';
+import slider2 from '../assets/slider2.jpg';
+import slider3 from '../assets/slider3.jpg';
+import storyImage from '../assets/storyImage.jpg';
+import eventImage1 from '../assets/eventImage1.jpg';
+import eventImage3 from '../assets/eventImage3.jpg';
+import student from '../assets/student.png';
+import family from '../assets/family.png';
+import volunteer from '../assets/volunteer.png';
+import woman from '../assets/woman.png';
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
-import UserChat from "../components/UserChat";
-import ChatBox from "../components/ChatBox";
-import { useNavigate } from 'react-router-dom'; // Import the useNavigate hook
+import { useNavigate } from 'react-router-dom';
+import '../assets/Chat.css'; // Import the custom CSS
 
-const Chat = () => {
+const Home = () => {
   const { user } = useContext(AuthContext);
   const { userChats, isUserChatsLoading, userChatsError, updateCurrentChat, potentialChats, createChat, onlineUsers } = useContext(ChatContext);
-  const navigate = useNavigate(); // Initialize the useNavigate hook
+  const navigate = useNavigate();
 
   const handleTextChatClick = () => {
-    navigate('/login'); // Navigate to the login page
+    navigate('/login');
   };
 
   return (
-    <div className="container-fluid chat-container">
-      {user ? (
-        <div className="users-column">
-          <div className="all-users p-3">
-            {isUserChatsLoading && <p>Loading Chats...</p>}
-            {userChats?.length > 0 ? (
-              userChats.map((chat, index) => (
-                <div key={index} onClick={() => updateCurrentChat(chat)}>
-                  <UserChat chat={chat} user={user} onlineUsers={onlineUsers} />
-                </div>
-              ))
-            ) : (
-              <p>No chats available</p>
-            )}
-            <h3>Potential Chats</h3>
-            {potentialChats.map((potentialChat, index) => (
-              <div key={index} className="user-card" onClick={() => createChat(user._id, potentialChat._id)}>
-                <UserChat chat={potentialChat} user={user} onlineUsers={onlineUsers} />
-              </div>
-            ))}
+    <>
+      <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={slider3} className="d-block w-100 carousel-image" alt="Slide 1" />
           </div>
         </div>
-      ) : null}
+      </div>
 
-      <div className="chat-column">
-        {user ? (
-          <div className="d-flex flex-column">
-            <div className="chat-header p-3">
-              <ChatBox />
+      <div style={{ backgroundColor: '#f7f8f9', padding: '20px 0' }}>
+        <div className="container mx-auto px-6">
+          <div className="row justify-content-center align-items-center">
+            <div className="col-md-5">
+              <h1 className="text-3xl font-bold text-gray-800 " style={{ color: 'black', fontWeight: 'bold' }}>
+                Story About<br />
+                <span style={{ color: '#ffc107', fontWeight: 'bold' }}>What We Do</span>
+              </h1>
+              <p className="text-gray-600 mt-4" style={{ color: 'black' }}>
+              Yasham works every single day to empower society holistically, through changes big or small. We work predominantly in the field of education to sharpen young minds.
+              </p>
+              <button type="button" className="btn btn-dark my-2" style={{ fontWeight: 'bold' }}>Know More</button>
             </div>
-          </div>
-        ) : (
-          <div className="min-vh-100 d-flex flex-column justify-content-center align-items-center text-white">
-            <div className="container">
+            <div className="col-md-7">
               <div className="row">
-                <div className="col-md-6 text-center mb-5" id="one">
-                  <h1 className="display-4 font-weight-bold">
-                    Talk to Anyone<br />Make friends!
-                  </h1>
-                  <p className="lead">
-                    Experience a random chat alternative to find friends, connect with people, and chat with strangers from all over the world!
-                  </p>
-                  <div className="d-flex justify-content-center mt-4">
-                    <button 
-                      className="btn btn-primary-custom btn-lg d-flex align-items-center"
-                      onClick={handleTextChatClick} // Add the onClick event handler
-                    >
-                      <FontAwesomeIcon icon={faCommentDots} className="mr-2" />
-                      <span style={{ marginLeft: '8px' }}>Text Chat</span>
-                    </button>
+                <div className="col-md-6 mb-4">
+                  <div className="text-center">
+                    <img src={student} className="mb-3" alt="Leave a Legacy" style={{ width: '12%', height: 'auto' }} />
+                    <h5 className="font-bold">Students and Youth</h5>
+                    <p>With over 9000+ students and youth impacted, we strive to empower the next generation.</p>
                   </div>
                 </div>
-                <div className="col-md-6 d-flex justify-content-center align-items-center">
-                  <img src={slider1} alt="Chat" className="img-fluid reduced-size" />
+                <div className="col-md-6 mb-4">
+                  <div className="text-center">
+                    <img src={family} className="mb-3" alt="Make Donation" style={{ width: '10%', height: 'auto' }} />
+                    <h5 className="font-bold">Families</h5>
+                    <p>With over 5000+ families assisted, we aim to provide resources and opportunities.</p>
+                  </div>
+                </div>
+                <div className="col-md-6 mb-4">
+                  <div className="text-center">
+                    <img src={volunteer} className="mb-3" alt="Become a Fundraiser" style={{ width: '10%', height: 'auto' }} />
+                    <h5 className="font-bold">Become a Fundraiser</h5>
+                    <p>Over 3000 women have associated with us, gaining opportunities for growth and self-reliance.</p>
+                  </div>
+                </div>
+                <div className="col-md-6 mb-4">
+                  <div className="text-center">
+                    <img src={volunteer} className="mb-3" alt="Become a Volunteer" style={{ width: '10%', height: 'auto' }} />
+                    <h5 className="font-bold">Become a Volunteer</h5>
+                    <p>
+                    Volunteer with us and be part of a mission making a real difference in underserved communities.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        )}
+        </div>
       </div>
-    </div>
+
+      <section className="demo-section py-5">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-4 mb-4 demoevent">
+              <img src={eventImage3} className="img-fluid" alt="Story" />
+            </div>
+
+            <div className="col-md-6 mb-4 mt-3">
+              <div className="p-4 h-100">
+                <h1 className="font-bold text-xl mb-3 text-dark" style={{ color: 'black', fontWeight: 'bold' }}>Name of the Event</h1>
+                <p className="mb-6 mt-3" style={{ color: 'black', fontSize: '1.2rem' }}>Industry. Lorem Ipsum has been the industry's standard dummy text ever since the, Lorem Ipsum has been the industry's standard dummy. Industry.</p>
+                <p className="mb-6 mt-3" style={{ color: 'black', fontSize: '1.2rem' }}>Industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+                <button className="btn btn-warning text-white mt-3" style={{ fontWeight: 'bold' }}>Learn More</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="events-section pt-4">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 mb-4 cardevent">
+              <div className="p-4 h-100">
+                <h1 className="font-bold text-xl mb-4 text-dark" style={{ fontWeight: 'bold' }}>Events:</h1>
+                <ul className="list-unstyled text-dark">
+                  <li className="mb-4 event-item ">
+                    <i className="fa fa-calendar mr-2"></i>
+                    <span className="event-item-text mx-4">Saathi Haath Badhana</span>
+                  </li>
+                  <hr className="event-divider" />
+                  <li className="mb-4 event-item">
+                    <i className="fa fa-calendar mr-2"></i>
+                    <span className="event-item-text mx-4">Hum Honge Kaamyab</span>
+                  </li>
+                  <hr className="event-divider" />
+                  <li className="mb-4 event-item">
+                    <i className="fa fa-calendar mr-2"></i>
+                    <span className="event-item-text mx-4">Swacch English Mission</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="col-md-8 mb-4">
+              <div className="p-4 h-100">
+                <h1 className="font-bold text-xl mb-3 text-dark" style={{ color: 'black', fontWeight: 'bold' }}>Upcoming Events</h1>
+                <p className="mb-6 mt-3" style={{ color: 'black', fontSize: '1.2rem' }}>Industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown industry. Lorem Ipsum has been the industry's standard dummy, Lorem Ipsum has been the industry's standard dummy. Industry.</p>
+
+                <button className="btn btn-warning text-white mt-3" style={{ fontWeight: 'bold' }}>Learn More</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="text-center my-5 join">
+        <h1 className="text-4xl font-bold mb-2" style={{ color: 'black', fontWeight: 'bold' }}>Become a Volunteer</h1>
+        <p className="text-xl mb-8" style={{ color: 'black', fontSize: '1.2rem' }}>Join us for a better life and beautiful future</p>
+        <button type="button" className="btn btn-light mt-1" style={{ fontWeight: 'bold' }}>Join Us Now</button>
+      </div>
+
+      <section className="bg-white video-section py-3 mb-4">
+        <div className="row mx-5">
+          <div className="col-md-5 mb-6 videoevent">
+            <iframe src="https://www.youtube.com/embed/mg9JoVv4w8E" title="video placeholder" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen className="w-100 h-100"></iframe>
+          </div>
+
+          <div className="col-md-7 my-4">
+            <div className="p-4 h-100">
+            <h1 className="font-bold text-xl mb-3 text-dark text-center" style={{ color: 'black', fontWeight: 'bold' }}>
+  Educate<span style={{ color: '#ffc107', fontWeight: 'bold' }}> Enlighten</span> Empower.
+</h1>
+              <p className="mb-6 mt-3 text-center" style={{ color: 'black', fontSize: '1.1rem' }}>To be able to guide and assist the many promising minds is one of the most gratifying feelings that motivates us at Yasham.
+                Yasham predominantly works in education to sharpen young minds today so that they might red-pencil the assigned labels.
+                To achieve our vision, we actively mediate between a child's aspirations and the acquisition of their goals.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
-export default Chat;
+export default Home;
