152bfe1b599d15d5d327fb67ed0ad02f28d9c9e9
Sachin Maurya <132452449+Sachin22424@users.noreply.github.com>
2025-01-01 21:57:56 +0530

    Update teamRoute.js

--- server_backend/Routes/teamRoute.js
+++ server_backend/Routes/teamRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const teamController = require('../controllers/teamController');
+const teamController = require('../Controllers/teamController');
 
 router.get('/', teamController.getAllMembers);
 router.post('/', teamController.createMember);
