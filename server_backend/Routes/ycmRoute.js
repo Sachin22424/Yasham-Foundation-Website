5a4de61ff18b9876a10e7c2401eee6b69af457d2
Sachin Maurya <132452449+Sachin22424@users.noreply.github.com>
2025-01-01 22:33:41 +0530

    Update ycmRoute.js

--- server_backend/Routes/ycmRoute.js
+++ server_backend/Routes/ycmRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getYCMInfo, updateYCMInfo } = require('../Controllers/ycmController.js');
 
-// Route to get YCM info
+// Route to get YCM info for thar
 router.get('/', getYCMInfo);
 
 // Route to update YCM info by ID
