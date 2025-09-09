4875df0f9fe50b3a1c7fe6fc62d424a1098870ad
Veerhan-glitch <83816165+Veerhan-glitch@users.noreply.github.com>
2025-09-09 23:06:27 +0530

    vit

--- client_frontend/vite.config.js
+++ client_frontend/vite.config.js
@@ -2,27 +2,40 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteCompression from 'vite-plugin-compression'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    // Gzip compression
     viteCompression({
+      verbose: true,       // log compressed files
+      disable: false,
+      threshold: 0,        // compress all files
       algorithm: 'gzip',
-      ext: '.gz',
+      ext: '.gz',          // output extension
+      deleteOriginFile: false, // keep original files
     }),
+    // Brotli compression
     viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 0,
       algorithm: 'brotliCompress',
       ext: '.br',
+      deleteOriginFile: false,
     })
   ],
   build: {
     minify: 'terser',
+    outDir: 'dist',        // make sure build goes to dist
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
           router: ['react-router-dom']
-        }
+        },
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
       }
     },
     chunkSizeWarningLimit: 1000,
